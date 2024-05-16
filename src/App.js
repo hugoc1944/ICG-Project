e6943df02286778c2fa47e864f973caaf04ff77f
hugoc1944 <125464948+hugoc1944@users.noreply.github.com>
2024-05-16 14:15:49 +0100

    Small fixes

--- src/App.js
+++ src/App.js
@@ -50,7 +50,41 @@ function Diamond(props) {
     </CubeCamera>
   )
 }
-
+function Radiant(props) {
+  const ref = useRef()
+  const { nodes } = useGLTF('/gem2.glb')
+  // Use a custom envmap/scene-backdrop for the diamond material
+  // This way we can have a clear BG while cube-cam can still film other objects
+  const texture = useLoader(RGBELoader, 'https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/aerodynamics_workshop_1k.hdr')
+  // Optional config
+  const config = useControls({
+    bounces: { value: 3, min: 0, max: 8, step: 1 },
+    aberrationStrength: { value: 0.01, min: 0, max: 0.1, step: 0.01 },
+    ior: { value: 2.75, min: 0, max: 10 },
+    fresnel: { value: 1, min: 0, max: 1 },
+    color: 'white',
+    fastChroma: true
+  })
+  return (
+    <CubeCamera resolution={256} frames={1} envMap={texture}>
+      {(texture) => (
+        <Caustics
+          backfaces
+          color={config.color}
+          position={[0, -0.5, 0]}
+          lightSource={[5, 5, -10]}
+          worldRadius={0.7}
+          ior={1}
+          backfaceIor={1.1}
+          intensity={0.1}>
+          <mesh castShadow ref={ref} geometry={nodes.Cylinder_009.geometry} scale={0.9} {...props}>
+            <MeshRefractionMaterial envMap={texture} {...config} toneMapped={false} />
+          </mesh>
+        </Caustics>
+      )}
+    </CubeCamera>
+  )
+}
 export default function App() {
   return (
     <Canvas shadows camera={{ position: [-5, 0.5, 5], fov: 45 }}>
@@ -59,6 +93,7 @@ export default function App() {
       <spotLight decay={0} position={[5, 5, -10]} angle={0.15} penumbra={1} />
       <pointLight decay={0} position={[-10, -10, -10]} />
       <Diamond rotation={[0, 0, 0.715]} position={[0, -0.175 + 0.5, 0]} />
+      <Radiant rotation={[0, 0, 0.8]} position={[0, -0.175 + 0.20, -3.5]} />
       <AccumulativeShadows
         temporal
         frames={100}
